0d212c3428f4b05621fcc9b6edfe3ee98c50deec
Edgar Bahilo <edgar.bahilo@b2-impact.com>
2024-08-06 11:45:39 +0200

    Adding basic test structure

--- mansa/linter.py
+++ mansa/linter.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse  # noqa: D100
 import ast
 import json
 import os
@@ -13,8 +13,9 @@ VALID_TAGS = {
     "technicalOwner": re.compile(r"[A-Z]{3}-[A-Za-z]+"),
     "businesUnit": re.compile(r"[A-Za-z]{3}"),
     "source": ["terraform", "portal", "amlsdkv2", "amlsdkv1"],
-    "ismsClassification": ["L", "M", "H"]
+    "ismsClassification": ["L", "M", "H"],
 }
+
 
 class MansaLinter(ast.NodeVisitor):
     """
@@ -41,10 +42,17 @@ class MansaLinter(ast.NodeVisitor):
         """
         ## TODO Fix
         if isinstance(node.func, ast.Name) and node.func.id in self.target_classes:
-            tags_arg = next((keyword for keyword in node.keywords if keyword.arg == 'tags'), None)
+            tags_arg = next((keyword for keyword in node.keywords if keyword.arg == "tags"), None)
             if not tags_arg:
                 error_code = self.target_classes[node.func.id]
-                self.errors.append((node.lineno, node.func.id, f"{error_code}: '{node.func.id}' instantiation is missing 'tags' argument"))
+                self.errors.append(
+                    (
+                        node.lineno,
+                        node.func.id,
+                        f"""{error_code}: '{node.func.id}'
+                     instantiation is missing 'tags' argument""",
+                    )
+                )
             else:
                 self.validate_tags(node.lineno, node.func.id, tags_arg.value)
         self.generic_visit(node)
@@ -79,7 +87,8 @@ class MansaLinter(ast.NodeVisitor):
             elif isinstance(valid_values, re.Pattern) and not valid_values.match(value_str):
                 self.errors.append((lineno, class_name, f"Invalid format for value '{value_str}' for key '{key_str}'"))
 
-def lint_code(code : str, config : dict) -> list:
+
+def lint_code(code: str, config: dict) -> list:
     """
     lint_code _summary_.
 
@@ -100,7 +109,8 @@ def lint_code(code : str, config : dict) -> list:
     linter.visit(tree)
     return linter.errors
 
-def lint_notebook(nb_path : str, config : dict)-> list:
+
+def lint_notebook(nb_path: str, config: dict) -> list:
     """
     lint_notebook _summary_.
 
@@ -116,16 +126,17 @@ def lint_notebook(nb_path : str, config : dict)-> list:
     list
         _description_
     """
-    with open(nb_path, 'r') as f:
+    with open(nb_path, "r") as f:
         notebook = json.load(f)
     errors = []
-    for cell in notebook['cells']:
-        if cell['cell_type'] == 'code':
-            code = ''.join(cell['source'])
+    for cell in notebook["cells"]:
+        if cell["cell_type"] == "code":
+            code = "".join(cell["source"])
             errors.extend(lint_code(code, config))
     return errors
 
-def scan_directory(directory : str) -> tuple[list, list]:
+
+def scan_directory(directory: str) -> tuple[list, list]:
     """
     scan_directory _summary_.
 
@@ -143,13 +154,14 @@ def scan_directory(directory : str) -> tuple[list, list]:
     ipynb_files = []
     for root, _, files in os.walk(directory):
         for file in files:
-            if file.endswith('.py'):
+            if file.endswith(".py"):
                 py_files.append(os.path.join(root, file))
-            elif file.endswith('.ipynb'):
+            elif file.endswith(".ipynb"):
                 ipynb_files.append(os.path.join(root, file))
     return py_files, ipynb_files
 
-def process_file(filepath : str, config : dict) -> list[tuple]:
+
+def process_file(filepath: str, config: dict) -> list[tuple]:
     """
     process_file _summary_.
 
@@ -165,22 +177,22 @@ def process_file(filepath : str, config : dict) -> list[tuple]:
     list[tuple]
         _description_
     """
-    if filepath.endswith('.py'):
-        with open(filepath, 'r') as file:
+    if filepath.endswith(".py"):
+        with open(filepath, "r") as file:
             code = file.read()
         lint_errors = lint_code(code, config)
         errors = [(filepath, lineno, name, message) for lineno, name, message in lint_errors]
-    elif filepath.endswith('.ipynb'):
+    elif filepath.endswith(".ipynb"):
         lint_errors = lint_notebook(filepath, config)
         errors = [(filepath, lineno, name, message) for lineno, name, message in lint_errors]
     return errors
 
-def main() -> None:
-    """Executes the linter via a cli invokation."""
 
-    parser = argparse.ArgumentParser(description='Custom Python Linter')
-    parser.add_argument('--directory', type=str, help='Directory to scan for Python and Jupyter files')
-    parser.add_argument('--config', type=str, help='Path to config.toml for configuration', default='mansa/config.toml')
+def main() -> None:
+    """Execute the linter via a cli invokation."""
+    parser = argparse.ArgumentParser(description="Custom Python Linter")
+    parser.add_argument("--directory", type=str, help="Directory to scan for Python and Jupyter files")
+    parser.add_argument("--config", type=str, help="Path to config.toml for configuration", default="mansa/config.toml")
     args = parser.parse_args()
 
     config_path = args.config
@@ -200,7 +212,7 @@ def main() -> None:
                 errors = future.result()
                 all_errors.extend(errors)
             except Exception as exc:
-                print(f'{filepath} generated an exception: {exc}')
+                print(f"{filepath} generated an exception: {exc}")
 
     for filepath, lineno, name, message in all_errors:
         if name:
@@ -208,5 +220,6 @@ def main() -> None:
         else:
             print(f"{filepath}:{lineno}: {message}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
---
+++ tests/test_linter.py
@@ -0,0 +1,23 @@
+import pytest  # noqa: D100
+
+from mansa.linter import MansaLinter
+
+## Teardown setup????
+
+@pytest.fixture
+def test_scan_directory() -> None:
+    return None
+
+
+def test_validate_tags() -> None:
+    return None
+
+def test_lint_code() -> None:
+    return None
+
+
+def test_lint_notebooks() -> None:
+    return None
+
+def test_process_file() -> None:
+    return None
